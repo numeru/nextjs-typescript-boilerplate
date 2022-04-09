a4fd4c6edc8069705264710f7a27e5295e4c1465
numeru <numeru@naver.com>
2022-04-09 20:05:05 +0900

    refactor : 테스트 이미지 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from 'next';
-import Image from 'next/image';
-import TestImage from 'assets/images/test.png';
 
 const Home: NextPage = () => {
 	return (
 		<div>
 			<>next</>
-			<Image src={TestImage} alt="test" />
 		</div>
 	);
 };
